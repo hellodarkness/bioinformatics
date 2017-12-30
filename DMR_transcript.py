5d3dfae5cd47ce3542cbd3074a488cc2b5cd4b5f
Jianing Sun <jianingsn@gmail.com>
2017-12-30 17:45:54 -0600

    Update DMR_transcript.py

--- DMR_transcript.py
+++ DMR_transcript.py
@@ -3,39 +3,80 @@
 #!/usr/bin/python3
 import pandas
 import matplotlib.pyplot as plt
- 
-df = pandas.read_table("/Users/lucydiamondsky/genomic/v27lift37.gpd",header=None)
-of = pandas.read_table("/Users/lucydiamondsky/genomic/DMR_new2.txt",header=None)
-df.ix[:,:1].groupby(1).count().plot.bar(legend=False)
-plt.show()
 
-result = {}
-for x in range(0,194):
-    dmr =  (of.ix[x,1] + of.ix[x,2])/2
-    for i in range(0,202697):
-        distance = 1000000
-        if of.ix[x,0] == df.ix[i,1]:
-            distance2 = abs(dmr - df.ix[i,3])
-            distance3 = abs(dmr - df.ix[i,4])
-            if distance2 < distance:
-                distance = distance2
-                if distance3 <distance:
-                    distance = distance3
-                    result[dmr] = [distance,df.ix[i,0]]
+df = pandas.read_table(
+    "/Users/lucydiamondsky/genomic/v27lift37.gpd", header=None)
+of = pandas.read_table(
+    "/Users/lucydiamondsky/genomic/DMR_new2.txt", header=None)
+tf = pandas.read_table(
+    "/Users/lucydiamondsky/genomic/tss", header=None)
+df.ix[:, :1].groupby(1).count().plot.bar(legend=False)
 
+#calculate distance to adjacent TSS
+result3 = {}
+for x in range(0, 194):
+    dmr = (of.ix[x, 1] + of.ix[x, 2]) / 2
+    distance = 1000000000
+    for i in range(0, 68798):
+        if (of.ix[x, 0] == tf.ix[i, 2]) and (tf.ix[i, 3] == '+'):
+            distance2 = abs(of.ix[x, 1] - tf.ix[i, 4])
+            distance3 = abs(of.ix[x, 2] - tf.ix[i, 4])
+            distance4 = min(distance2, distance3)
+            if distance4 < distance:
+                distance = distance4
+                result3[dmr] = [of.ix[x, 0], distance, tf.ix[i, 1]]
+        if (of.ix[x, 0] == tf.ix[i, 2]) and (tf.ix[i, 3] == '-'):
+            distance2 = abs(of.ix[x, 1] - tf.ix[i, 5])
+            distance3 = abs(of.ix[x, 2] - tf.ix[i, 5])
+            distance4 = min(distance2, distance3)
+            if distance4 < distance:
+                distance = distance4
+                result3[dmr] = [of.ix[x, 0], distance, tf.ix[i, 1]]
+
+for v in result3.values():
+    print(format(v))
+
+for v, k in result3.items():
+    print('{v} {k}'.format(v=v, k=k))
+
+#calculate distance to adjacent transcript
 result = {}
-for x in range(0,5):
-    dmr =  (of.ix[x,1] + of.ix[x,2])/2
-    for i in range(0,202696):
-        distance = 1000000
-        if of.ix[x,0] == df.ix[i,1]:
-            if dmr > df.ix[i,3] and dmr < df.ix[i,4]:
-                result[dmr] = ['the dmr is within transcript',df.ix[i,0]
+for x in range(0, 195):
+    dmr = (of.ix[x, 1] + of.ix[x, 2]) / 2
+    distance = 1000000000
+    for i in range(0, 202696):
+        if of.ix[x, 0] == df.ix[i, 1]:
+            if of.ix[x, 1] > df.ix[i, 3] and of.ix[x, 2] < df.ix[i, 4]:
+                result[dmr] = [of.ix[x, 0],
+                               'the dmr is located within transcript', df.ix[i, 0]]
             else:
-                distance2 = abs(dmr - df.ix[i,3])
-                distance3 = abs(dmr - df.ix[i,4])
+                distance2 = abs(dmr - df.ix[i, 3])
+                distance3 = abs(dmr - df.ix[i, 4])
                 if distance2 < distance:
                     distance = distance2
-                    if distance3 <distance:
-                        distance = distance3
-                        result[dmr] = [distance,df.ix[i,0]]
+                if distance3 < distance:
+                    distance = distance3
+                    result[dmr] = [of.ix[x, 0], distance, df.ix[i, 0]]
+
+result1 = {}
+for x in range(0, 195):
+    dmr = (of.ix[x, 1] + of.ix[x, 2]) / 2
+    for i in range(0, 202696):
+        if of.ix[x, 0] == df.ix[i, 1]:
+            if of.ix[x, 2] > df.ix[i, 3] and of.ix[x, 1] < df.ix[i, 3]:
+                result1[dmr] = [of.ix[x, 0],
+                                'the dmr is overlap with transcript', df.ix[i, 0]]
+            if of.ix[x, 2] > df.ix[i, 4] and of.ix[x, 1] < df.ix[i, 4]:
+                result1[dmr] = [of.ix[x, 0],
+                                'the dmr is overlap with transcript', df.ix[i, 0]]
+
+for v in result3.values():
+    print(format(v))
+
+for v, k in result3.items():
+    print('{v} {k}'.format(v=v, k=k))
+
+transcript = {}
+for x in range(0, 194):
+    result3.values()[x][2]
+    transcript[x] = [result.values()[x][2]]
