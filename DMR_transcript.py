3cc18fef76a341b2e3069ec69f1e747491ceac4b
Jianing Sun <jianingsn@gmail.com>
2017-12-30 17:49:56 -0600

    Update DMR_transcript.py

--- DMR_transcript.py
+++ DMR_transcript.py
@@ -1,5 +1,8 @@
 //calculate the distance of differentially methylated site (DMR) and transcript in reference genome
-
+//http://genome.ucsc.edu/cgi-bin/hgTables?hgsid=605193253_5uQOh6j9UyM6veEM8NZgpGxg7Lun
+//transcript file achieved from reference genome
+//browser session: http://epigenomegateway.wustl.edu/browser/?genome=hg19&session=EI1EV6BG4w&statusId=100728609
+        
 #!/usr/bin/python3
 import pandas
 import matplotlib.pyplot as plt
